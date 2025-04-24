0e9544e0af6457d81a6d505e64a2af96f045214f
Ramisa Anan <72885540+Ramisanan@users.noreply.github.com>
2025-04-24 12:51:56 -0400

    2nd push

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.upload_cookbook, name='upload_cookbook'),
     path('preferences/', views.set_preferences, name='set_preferences'),
+
+    # Show all recipes
+    path('grocery-list/', views.recipe_list_view, name='recipe_list'),
+
+    # Dynamic grocery list fetch by recipe name
+    path('grocery-list/<str:recipe_name>/', views.grocery_list_view, name='grocery_list'),
 ]
+
+
